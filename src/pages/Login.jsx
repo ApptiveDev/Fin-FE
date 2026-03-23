6b17d141660ceec3cd0015d9d2d469a90e66641a
yeeun <143855139+badyeun@users.noreply.github.com>
2026-03-23 22:17:25 +0900

    fix: 캐시 초기화

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -54,7 +54,7 @@ function KakaoButton() {
       onClick={handleKakaoLogin}
       className="flex items-center justify-center gap-2 bg-[#FEE500] hover:bg-yellow-200 text-[#51515] py-2.5 rounded-full transition-colors">
       <img src={kakaoIcon} alt="카카오" className="w-5" />
-      <span className="font-[Inter] font-semibold text-[#515151] relative top-[1px]">카카오로 시작하기</span>
+      <span className="font-[Inter] font-semibold text-[#515151] relative top-px">카카오로 시작하기</span>
     </button>
   )
 }
@@ -68,7 +68,7 @@ function GoogleButton() {
     onClick={handleGoogleLogin}
     className="flex items-center justify-center gap-2 bg-white hover:bg-gray-100 text-[#51515] py-2.5 rounded-full transition-colors">
       <img src={googleIcon} alt="구글" className="w-5 h-5" />
-      <span className="font-[Inter] font-semibold text-[#515151]  relative top-[1px]">구글로 시작하기</span>
+      <span className="font-[Inter] font-semibold text-[#515151]  relative top-px">구글로 시작하기</span>
     </button>
   )
 }
